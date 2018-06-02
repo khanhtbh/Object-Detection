d32d9143946433a77b52cb2e7af966b3281461f2
Khanh Trinh <khanh.tbh@gmail.com>
2018-06-02 15:05:02 +0700

    Initial Commit

---
+++ ObjectDetection/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ObjectDetection
+//
+//  Created by Kei on 6/2/18.
+//  Copyright © 2018 Kei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
